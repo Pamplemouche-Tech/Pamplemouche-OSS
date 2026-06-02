d648826087a71e05e7d93a1f4cc191d3d8203ed6
pamplemouche <pamplemouchetech@gmail.com>
2026-06-02 06:41:32 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include "uefi.h"
 
-// Implémentation maison de memcpy requise par le compilateur pour les initialisations de tableaux
+// Implémentation maison de memcpy requise par le compilateur
 void *memcpy(void *dest, const void *src, UINTN n) {
     unsigned char *d = (unsigned char *)dest;
     const unsigned char *s = (const unsigned char *)src;
@@ -11,18 +11,38 @@ void *memcpy(void *dest, const void *src, UINTN n) {
 }
 
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-    // Chaîne UTF-16 terminée par un caractère nul (0)
-    // \r\n est le retour à la ligne standard pour l'UEFI
+    // Message de démarrage
     uint16_t message[] = {
         'C', 'e', 'r', 'v', 'e', 'a', 'u', ' ', 'e', 't', ' ', 
         'M', 'a', 'i', 'n', 's', ' ', 'o', 'p', 'e', 'r', 'a', 't', 'i', 'o', 'n', 'n', 'e', 'l', 's', '.',
         '\r', '\n', 0
     };
-
-    // On demande à l'UEFI d'afficher la chaîne à l'écran
     SystemTable->ConOut->OutputString(SystemTable->ConOut, message);
 
-    // On fige le processeur dans une boucle basse consommation pour observer le résultat
+    // Variables requises pour stocker la carte mémoire
+    UINTN memory_map_size = 0;
+    EFI_MEMORY_DESCRIPTOR *memory_map = (void*)0; // Tampon nul au départ pour demander la taille requise
+    UINTN map_key = 0;
+    UINTN descriptor_size = 0;
+    unsigned int descriptor_version = 0;
+
+    // Premier appel à GetMemoryMap avec un tampon de 0 pour obtenir la taille réelle nécessaire dans `memory_map_size`
+    SystemTable->BootServices->GetMemoryMap(
+        &memory_map_size, 
+        memory_map, 
+        &map_key, 
+        &descriptor_size, 
+        &descriptor_version
+    );
+
+    // Message pour confirmer que l'analyse de la mémoire a commencé
+    uint16_t mem_message[] = {
+        'C', 'a', 'r', 't', 'e', ' ', 'm', 'e', 'm', 'o', 'i', 'r', 'e', ' ', 'a', 'n', 'a', 'l', 'y', 's', 'e', 'e', '.',
+        '\r', '\n', 0
+    };
+    SystemTable->ConOut->OutputString(SystemTable->ConOut, mem_message);
+
+    // On fige le processeur
     while(1) {
         __asm__ __volatile__("hlt");
     }
