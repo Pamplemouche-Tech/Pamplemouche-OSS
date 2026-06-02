55c6c3b5e8719736875741c66f76cc6b91305852
pamplemouche <pamplemouchetech@gmail.com>
2026-06-02 06:41:09 +0200

    Update uefi.h

--- uefi.h
+++ uefi.h
@@ -25,12 +25,43 @@ typedef struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
     EFI_TEXT_STRING OutputString;
 } EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
 
+// Structure décrivant un descripteur de mémoire UEFI
+typedef struct {
+    unsigned int Type;
+    unsigned int Padding;
+    unsigned long long PhysicalStart;
+    unsigned long long VirtualStart;
+    unsigned long long NumberOfPages;
+    unsigned long long Attribute;
+} EFI_MEMORY_DESCRIPTOR;
+
+// Déclaration des Boot Services (les services disponibles avant de lancer le noyau)
+typedef struct {
+    char Header[24];
+    void *RaiseTPL;
+    void *RestoreTPL;
+    void *AllocatePages;
+    void *FreePages;
+    // Fonction critique pour récupérer la carte de la mémoire RAM
+    EFI_STATUS (*GetMemoryMap)(
+        UINTN *MemoryMapSize,
+        EFI_MEMORY_DESCRIPTOR *MemoryMap,
+        UINTN *MapKey,
+        UINTN *DescriptorSize,
+        unsigned int *DescriptorVersion
+    );
+    // ... Le reste des Boot Services est ignoré pour le moment
+} EFI_BOOT_SERVICES;
+
 // La table système principale (fournie par le firmware au démarrage)
 typedef struct {
     char Header[24];
     void *ConIn;
     void *ConInTo;
     EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut;
+    void *StandardError;
+    void *In;
+    EFI_BOOT_SERVICES *BootServices; // Ajout de l'accès aux Boot Services
 } EFI_SYSTEM_TABLE;
 
 #endif // UEFI_H
